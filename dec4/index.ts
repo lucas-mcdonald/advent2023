23b8bf8b820cbbb25f1c4fce5c3473cdffb7fcf3
Lucas McDonald <lucmcd10@gmail.com>
2023-12-04 11:57:43 -0500

    Add comments

--- dec4/index.ts
+++ dec4/index.ts
@@ -1,10 +1,15 @@
 import { readFileSync } from 'fs';
 
+// Read the input file and split it into lines
 const lines = readFileSync('input.txt').toString().split('\n');
 
+// Map each line to a scratch card object
 const scratchCards = lines.map((line) => {
+  // Split the line into card title and numbers
   const [cardTitle, numbers] = line.split(': ').map((chunk) => chunk.trim());
+  // Extract the card number from the title
   const cardNumber = parseInt(cardTitle.split(' ')[1]);
+  // Split the numbers into winners and ours, and convert them to integer arrays
   const [winners, ours] = numbers
     .split('|')
     .map((chunk) => chunk.trim())
@@ -14,6 +19,7 @@ const scratchCards = lines.map((line) => {
         .map((num) => parseInt(num))
         .filter((num) => !isNaN(num))
     );
+  // Return the scratch card object
   return {
     cardNumber,
     winners,
@@ -24,8 +30,10 @@ const scratchCards = lines.map((line) => {
   };
 });
 
+// For each scratch card, calculate the points and matches
 scratchCards.forEach((card) => {
   card.winners.forEach((winner) => {
+    // If our numbers include the winner, increment matches and calculate points
     if (card.ours.includes(winner)) {
       card.matches++;
       card.points = card.points === 0 ? 1 : card.points * 2;
@@ -34,12 +42,14 @@ scratchCards.forEach((card) => {
 });
 
 // Part 2
+// For each scratch card, calculate the copies based on matches
 scratchCards
   .map((card) => card.matches)
   .forEach((points, index) => {
     if (points === 0) {
       return;
     }
+    // Add copies to the following cards based on the number of matches
     for (let i = index + 1; i < index + points + 1; i++) {
       if (i >= scratchCards.length) {
         break;
@@ -49,10 +59,12 @@ scratchCards
     }
   });
 
+// Log the total points for Part 1
 console.log(
   'Part 1: ',
   scratchCards.map((card) => card.points).reduce((a, b) => a + b, 0)
 );
+// Log the total copies for Part 2
 console.log(
   'Part 2: ',
   scratchCards.map((card) => card.copies).reduce((a, b) => a + b)
