df31b0bb7e575347e1f3a071988d9f23c0470cf2
Lucas McDonald <lucmcd10@gmail.com>
2023-12-06 09:43:46 -0500

    added quadratic solution w runtimes

--- dec6/quadratic.ts
+++ dec6/quadratic.ts
@@ -6,35 +6,34 @@ const [times, distances] = readFileSync('./input.txt', 'utf8')
   .split('\n')
   .map((line) => line.split(':').pop()?.trim().split(/\s+/).map(Number)) as [number[], number[]];
 
-// Part 1
-const getRecordBreakers = (time: number, distance: number) => {
-    const recordBreakers = [];
-    for (let holdTime = 0; holdTime < time; holdTime++) {
-        const speed = holdTime;
-        const distanceTravelled = speed * (time - holdTime);
-        if (distanceTravelled > distance) {
-            recordBreakers.push(holdTime);
-        }
+const getRoots = (a: number, b: number, c: number) => {
+    const discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+        return [];
     }
-    return recordBreakers;
-};
+    const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return [root1, root2];
+}
 
+//Part 1
 const numRecordBreakers = [];
 for (let i = 0; i < times.length; i++) {
     const time = times[i];
     const distance = distances[i];
-    numRecordBreakers.push(getRecordBreakers(time, distance).length);
+    const roots = getRoots(-1, time, -distance);
+    numRecordBreakers.push(Math.abs(Math.floor(roots[0]) - Math.floor(roots[1])));
 }
 
 // Part 2
 const newTime = parseInt(times.map(String).join(''));
 const newDistance = parseInt(distances.map(String).join(''));
-
+const newRoots = getRoots(-1, newTime, -newDistance);
 
 // Output results
 console.log('Part 1:', numRecordBreakers.reduce((a, b) => a * b, 1));
-console.log('Part 2:', getRecordBreakers(newTime, newDistance).length);
+console.log('Part 2:', Math.abs(Math.floor(newRoots[0]) - Math.floor(newRoots[1])));
 const end = performance.now();
 console.log(`Runtime: ${end - start}ms`);
 
-// Runtime: 148.952666ms
+// Runtime: 0.32424999999999926ms
