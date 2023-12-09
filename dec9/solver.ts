f16946a7b71338b355c22755f8b49415e003f630
Lucas McDonald <lucmcd10@gmail.com>
2023-12-09 10:53:18 -0500

    part1 and 2 complete (recursion)

---
+++ dec9/solver.ts
@@ -0,0 +1,49 @@
+const parseInput = (input: string): number[][] => {
+    return input.split('\n').map(line => line.split(/\s+/).map(num => parseInt(num))) ;
+}
+
+const getDx = (sequence: number[]): number[] => {
+    let dx = [];
+    for (let i = 1; i < sequence.length; i++) {
+        dx.push(sequence[i] - sequence[i - 1]);
+    }
+    return dx;
+}
+
+const extrapolate = (sequence: number[]): number[] => {
+    let extrapolated = [...sequence];
+    let dx = getDx(extrapolated);
+    if (dx.every(d => d === 0)) {
+        extrapolated.push(extrapolated[extrapolated.length - 1]);
+    } else {
+        let last = extrapolated[extrapolated.length - 1] + extrapolate(dx)[dx.length];
+        extrapolated.push(last)
+    }
+    return extrapolated;
+}
+
+const runExtrapolation = (sequences: number[][]): number => {
+    const nextValues: number[] = [];
+    for (let i = 0; i < sequences.length; i++) {
+        let dx = getDx(sequences[i]);
+        const extrapolated = extrapolate(sequences[i]);
+        // console.log(extrapolated);
+        nextValues.push(extrapolated[extrapolated.length - 1]);
+    }
+    return nextValues.reduce((a, b) => a + b, 0);
+}
+
+const part1 = (input: string): number => {
+    const sequences = parseInput(input);
+    return runExtrapolation(sequences);
+}
+
+const part2 = (input: string): number => {
+    const sequences = parseInput(input).map(sequence => sequence.toReversed());
+    return runExtrapolation(sequences);
+}
+
+export default {
+    part1,
+    part2
+}
