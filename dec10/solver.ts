e141108124a27c40cfe53d7929274396f4d366c2
Lucas McDonald <lucmcd10@gmail.com>
2023-12-10 22:22:58 -0500

    part 2 wip

--- dec10/solver.ts
+++ dec10/solver.ts
@@ -1,3 +1,6 @@
+import { on } from 'events';
+import { relative } from 'path';
+
 class Point {
   char: string;
   x: number;
@@ -8,10 +11,11 @@ class Point {
     this.y = y;
   }
   getNeighbours(lines: string[]) {
-    const maxIndex = lines[0].length - 1;
+    const xMax = lines[0].length - 1;
+    const yMax = lines.length - 1;
     const neighbours: [number, number][] = [
-      [Math.min(this.x + 1, maxIndex), this.y],
-      [this.x, Math.min(this.y + 1, maxIndex)],
+      [Math.min(this.x + 1, yMax), this.y],
+      [this.x, Math.min(this.y + 1, xMax)],
       [Math.max(this.x - 1, 0), this.y],
       [this.x, Math.max(this.y - 1, 0)],
     ];
@@ -24,36 +28,30 @@ class Point {
 
     const dx = point.x - this.x;
     const dy = point.y - this.y;
-    // console.log('dx: ', dx, ' dy: ', dy)
-    // console.log('Point A: ', this.toString());
-    // console.log('Point B: ', point.toString());
     if (dx === 1 && dy === 0) {
-      // console.log('Case [1, 0] - ', 'A: ', charA, ' B: ', charB);
-      // console.log(['|', 'F', '7', 'S'].includes(charA));
-      // console.log(['|', 'J', 'L', 'S'].includes(charB));
       return (
         ['|', 'F', '7', 'S'].includes(charA) &&
         ['|', 'J', 'L', 'S'].includes(charB)
       );
     } else if (dx === -1 && dy === 0) {
-      // console.log('Case [-1, 0] - ', 'A: ', charA, ' B: ', charB);
       return (
         ['|', 'J', 'L', 'S'].includes(charA) &&
         ['|', 'F', '7', 'S'].includes(charB)
       );
     } else if (dx === 0 && dy === 1) {
-      // console.log('Case [0, 1] - ', 'A: ', charA, ' B: ', charB);
       return (
         ['-', 'F', 'L', 'S'].includes(charA) &&
         ['-', 'J', '7', 'S'].includes(charB)
       );
     } else if (dx === 0 && dy === -1) {
-      // console.log('Case [0, -1] - ', 'A: ', charA, ' B: ', charB);
       return (
         ['-', 'J', '7', 'S'].includes(charA) &&
         ['-', 'F', 'L', 'S'].includes(charB)
       );
     }
+  }
+  copy() {
+    return new Point(this.char, this.x, this.y);
   }
 
   toString() {
@@ -108,46 +106,155 @@ const parseInput = (input: string) => {
   return [startingPoint, lines] as const;
 };
 
-const part1 = (input: string) => {
+const buildLoop = (input: string) => {
   const [startingPoint, lines] = parseInput(input);
   let lastPoint: Point | null = null;
   let currentPoint = startingPoint;
   let neighbours = [];
   let nextCoords: [number, number];
-  let steps = 0;
+  let loop = [];
   do {
     neighbours = currentPoint.getNeighbours(lines);
-    nextCoords = neighbours.find(
-      (neighbour) => {
-        // console.log('Neighbour: ', neighbour);
-        return currentPoint.isConnected(
+    lastPoint = loop[loop.length - 1];
+    nextCoords = neighbours.find((neighbour) => {
+      // console.log('Neighbour: ', neighbour);
+      return (
+        currentPoint.isConnected(
           new Point(
             lines[neighbour[0]][neighbour[1]],
             neighbour[0],
             neighbour[1]
           )
         ) &&
-        (lastPoint === null || neighbour[0] !== lastPoint?.x || neighbour[1] !== lastPoint?.y)
-          }) as [number, number];
-    lastPoint = currentPoint;
+        (lastPoint === undefined ||
+          neighbour[0] !== lastPoint?.x ||
+          neighbour[1] !== lastPoint?.y)
+      );
+    }) as [number, number];
+    loop.push(currentPoint.copy());
     currentPoint = new Point(
       lines[nextCoords[0]][nextCoords[1]],
       nextCoords[0],
       nextCoords[1]
     );
-    steps++;
   } while (currentPoint.char !== 'S');
 
-  console.log('Last: ', lastPoint.toString());
-  console.log('Current: ', currentPoint.toString());
-  return steps / 2;
+  return loop;
 };
 
+function replaceAt(string: string, index: number, replacement: string) {
+  return (
+    string.slice(0, index) +
+    replacement +
+    string.slice(index + replacement.length)
+  );
+}
+
+const part1 = (input: string) => {
+  const loop = buildLoop(input);
+  return loop.length / 2;
+};
+
+// const shoestring = (points: Point[]) => {
+//   let area = 0;
+//   for (let i = 0; i < points.length; i++) {
+//     area += points[i].x * points[(i + 1) % points.length].y - points[(i + 1) % points.length].x * points[i].y;
+//   }
+//   return area / 2;
+// }
 const part2 = (input: string) => {
-  return 0;
+  const loop = buildLoop(input);
+  const lines = input.split('\n');
+  //Replace S
+  const start = loop[0];
+  const relativeNeighbours = start
+    .getNeighbours(lines)
+    .filter((neighbour) =>
+      start.isConnected(
+        new Point(lines[neighbour[0]][neighbour[1]], neighbour[0], neighbour[1])
+      )
+    )
+    .map(
+      (neighbour) => `(${neighbour[0] - start.x}, ${neighbour[1] - start.y})`
+    );
+  if (relativeNeighbours.length !== 2)
+    throw new Error('Invalid starting point');
+
+  if (
+    relativeNeighbours.includes('(1, 0)') &&
+    relativeNeighbours.includes('(0, 1)')
+  ) {
+    start.char = 'F';
+  } else if (
+    relativeNeighbours.includes('(1, 0)') &&
+    relativeNeighbours.includes('(0, -1)')
+  ) {
+    start.char = '7';
+  } else if (
+    relativeNeighbours.includes('(0, -1)') &&
+    relativeNeighbours.includes('(0, 1)')
+  ) {
+    start.char = '-';
+  } else if (
+    relativeNeighbours.includes('(-1, 0)') &&
+    relativeNeighbours.includes('(1, 0)')
+  ) {
+    start.char = '|';
+  } else if (
+    relativeNeighbours.includes('(-1, 0)') &&
+    relativeNeighbours.includes('(-1, 0)')
+  ) {
+    start.char = 'J';
+  } else if (
+    relativeNeighbours.includes('(-1, 0)') &&
+    relativeNeighbours.includes('(0, 1)')
+  ) {
+    start.char = 'L';
+  }
+  loop[0].char = start.char;
+  lines[start.x] = replaceAt(lines[start.x], start.y, start.char);
+
+  // Find area
+  let output = [];
+  let area = 0;
+  for (let i = 0; i < lines.length; i++) {
+    let inside = false;
+    let outputLine = [];
+    let lastEdgeIndex = 0;
+    for (let j = 0; j < lines[i].length; j++) {
+      if (loop.findIndex((point) => point.x === i && point.y === j) !== -1) {
+        if (
+          (!inside && ['|', 'J', '7'].includes(lines[i][j])) ||
+          (inside && ['|', 'L', 'F'].includes(lines[i][j]))
+        ) {
+          inside = !inside;
+        }
+        outputLine.push(lines[i][j]);
+        lastEdgeIndex = j;
+      } else {
+        if (inside) {
+          area++;
+          outputLine.push('#');
+        } else {
+          outputLine.push(lines[i][j]);
+        }
+      }
+    }
+    if (inside) area -= lines[i].length - lastEdgeIndex - 1;
+    output.push(outputLine);
+  }
+  console.log(
+    output
+      .map((line) => line.join())
+      .join('\n')
+      .replace(/,/g, '')
+  );
+  return area;
 };
 
 export default {
   part1,
   part2,
 };
+
+// Wrong 1717, 1309, 1668, 1096, 913, 515, 455
