6a12fd6bcef4e696cdf38c58de3947603450aec6
Lucas McDonald <lucmcd10@gmail.com>
2023-12-12 09:07:30 -0500

    init day 12

---
+++ dec12/solver.ts
@@ -0,0 +1,17 @@
+const parseInput = (input: string) => {
+  const lines = input.split('\n');
+  return lines;
+};
+
+const part1 = (input: string) => {
+  return 0;
+};
+
+const part2 = (input: string) => {
+  return 0;
+};
+
+export default {
+  part1,
+  part2,
+};
